5c0d7cab7c8b1b0f53cbc9133a55b916aa128c17
Clemuch009 <clementmuchaimoko@gmail.com>
2026-06-25 16:53:24 +0300

    region

--- analysis/structure_engine/region_classifier.py
+++ analysis/structure_engine/region_classifier.py
@@ -10,7 +10,7 @@ from analysis.structure_engine.line_model import LineObject
 # Contract
 # ---------------------------------<
 
-print("latest deplyment running"
+print("latest deplyment running")
 class RegionResult(TypedDict):
     region_type: str        # table_candidate | structured_block | unstructured
     start_line:  int
